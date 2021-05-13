0fc768accfe307641b6d5d85383d202e0a318230
Hauke <hauke.diers@outlook.com>
2021-05-13 17:45:37 +0200

    Dialogflow example app is running

--- src/App.js
+++ src/App.js
@@ -1,74 +1,205 @@
 import React, {Component} from 'react';
 import '@progress/kendo-theme-default/dist/all.css';
 import './App.css';
-import { Calendar } from '@progress/kendo-react-dateinputs';
-import { Chat } from '@progress/kendo-react-conversational-ui';
+import { Chat, HeroCard } from "@progress/kendo-react-conversational-ui";
+import { Calendar } from "@progress/kendo-react-dateinputs";
+import { ApiAiClient } from "api-ai-javascript";
+const client = new ApiAiClient({
+  accessToken: "280344fb165a461a8ccfef7e1bb47e65",
+});
+const user = {
+  id: 1,
+  name: "John",
+};
+const bot = {
+  id: "botty",
+  name: "Botty McbotFace",
+  avatarUrl: "https://demos.telerik.com/kendo-ui/content/chat/InsuranceBot.png",
+};
 
-class App extends Component {
+const App = () => {
+  const [messages, setMessages] = React.useState([]);
+  const onResponse = React.useCallback(
+    (activity) => {
+      let responseMessages = [];
+      activity.result.fulfillment.messages.forEach(function (element) {
+        let newMessage;
+        newMessage = {
+          text: element.speech,
+          author: bot,
+          timestamp: new Date(activity.timestamp),
+          suggestedActions: element.replies
+            ? element.replies.map((x) => {
+                return {
+                  type: "reply",
+                  title: x,
+                  value: x,
+                };
+              })
+            : [],
+        };
+        responseMessages.push(newMessage);
+      });
+      setMessages((oldMessages) => [...oldMessages, ...responseMessages]);
 
-constructor(props) {
-    super(props);
-    this.user = {
-      id: 1,
-      avatarUrl: "https://via.placeholder.com/24/008000/008000.png",
-    };
-    this.bot = { id: 0 };
-    this.state = {
-      messages: [
-        {
-          author: this.bot,
-          suggestedActions: [
-            {
-              type: "reply",
-              value: "Oh, really?",
-            },
-            {
-              type: "reply",
-              value: "Thanks, but this is boring.",
-            },
-          ],
-          timestamp: new Date(),
-          text: "Hello, this is a demo bot. I don't do much, but I can count symbols!",
-        },
-      ],
-    };
-  }
+      if (activity.result.fulfillment.data) {
+        let newMessage;
+        newMessage = {
+          text: "",
+          author: bot,
+          timestamp: new Date(activity.timestamp),
+          suggestedActions: activity.result.fulfillment.data.null
+            .suggestedActions
+            ? parseActions(
+                activity.result.fulfillment.data.null.suggestedActions
+              )
+            : [],
+          attachments: activity.result.fulfillment.data.null.attachments
+            ? activity.result.fulfillment.data.null.attachments
+            : [],
+        };
+        setMessages((oldMessages) => [...oldMessages, newMessage]);
+      }
 
-  addNewMessage = (event) => {
-    let botResponce = Object.assign({}, event.message);
-    botResponce.text = this.countReplayLength(event.message.text);
-    botResponce.author = this.bot;
-    this.setState((prevState) => ({
-      messages: [...prevState.messages, event.message],
-    }));
-    setTimeout(() => {
-      this.setState((prevState) => ({
-        messages: [...prevState.messages, botResponce],
-      }));
-    }, 1000);
-  };
+      if (
+        activity.result.fulfillment.speech ===
+        "When do you want your insurance to start?"
+      ) {
+        let newAttachments = [
+          {
+            type: "calendar",
+          },
+        ];
+        setMessages([
+          ...messages,
+          {
+            author: bot,
+            timestamp: new Date(activity.timestamp),
+            attachments: newAttachments,
+          },
+        ]);
+      }
+    },
+    [messages]
+  );
+  React.useEffect(() => {
+    client.eventRequest("Welcome").then(onResponse);
+  }, []);
 
-  countReplayLength = (question) => {
-    let length = question.length;
-    let answer = question + " contains exactly " + length + " symbols.";
-    return answer;
-  };
+  const аttachmentTemplate = (props) => {
+    let attachment = props.item;
 
-	render() {
-	  return (
-	    <div className="App">
-	      <h1>Hello KendoReact!</h1>
-	      <Calendar/>
-        <Chat
-          user={this.user}
-          messages={this.state.messages}
-          onMessageSend={this.addNewMessage}
-          placeholder={"Type a message..."}
-          width={400}
+    if (attachment.type === "quote") {
+      return (
+        <div className="k-card k-card-type-rich">
+          <div className="k-card-body">
+            <div>
+              <strong>Type:</strong>
+              <span>{attachment.coverage}</span>
+            </div>
+            <div>
+              <strong>Car model:</strong>
+              <span>{attachment.make}</span>
+            </div>
+            <div>
+              <strong>Car cost:</strong>
+              <span>{attachment.worth}</span>
+            </div>
+            <div>
+              <strong>Start date:</strong>
+              <span>{attachment.startDate}</span>
+            </div>
+            <hr />
+            <div>
+              <strong>Total:</strong>
+              <span>{attachment.premium}</span>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (attachment.type === "payment_plan") {
+      return (
+        <div className="k-card k-card-type-rich">
+          <div className="k-card-body">
+            {attachment.rows.map((row, index) => (
+              <div key={index}>{row.text}</div>
+            ))}
+            <hr />
+            <div>
+              <strong>Total:</strong>
+              <span>{attachment.premium}</span>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (attachment.type === "calendar") {
+      return (
+        <Calendar
+          onChange={(event) => {
+            addNewMessage(event);
+          }}
         />
-	    </div>
-	  );
-	}
-}
+      );
+    }
+
+    return (
+      <HeroCard
+        title={attachment.title}
+        imageUrl={attachment.images ? attachment.images[0].url : ""}
+        subtitle={attachment.subtitle ? attachment.subtitle : ""}
+        actions={attachment.buttons}
+        onActionExecute={addNewMessage}
+      />
+    );
+  };
+
+  const parseActions = (actions) => {
+    if (actions !== undefined) {
+      actions.map((action) => {
+        if (action.type === "postBack") {
+          action.type = "reply";
+        }
+      });
+      return actions;
+    }
+
+    return [];
+  };
+
+  const parseText = (event) => {
+    if (event.action !== undefined) {
+      return event.action.value;
+    } else if (event.value) {
+      return event.value;
+    } else {
+      return event.message.text;
+    }
+  };
+
+  const addNewMessage = (event) => {
+    let value = parseText(event);
+    client.textRequest(value.toString()).then(onResponse, this);
+
+    if (!event.value) {
+      setMessages([
+        ...messages,
+        {
+          author: user,
+          text: value,
+          timestamp: new Date(),
+        },
+      ]);
+    }
+  };
+
+  return (
+    <Chat
+      messages={messages}
+      user={user}
+      onMessageSend={addNewMessage}
+      attachmentTemplate={аttachmentTemplate}
+    />
+  );
+};
 
 export default App;
